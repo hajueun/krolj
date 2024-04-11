6ff7312f3cbea23acb1b709261a576911c646086
hajueun <dntru8520@naver.com>
2024-04-11 17:41:49 +0900

    first-commit

---
+++ pages/posts/first-page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+export default function FirstPost() {
+    return (
+        <>
+        <h1 style={{fontSize: "60px"}}>FIRST PAGE</h1>
+        <p>안녕하세요, 저는 작년에 따복한 23학번 하주은입니다.</p>
+        <p style={{color: "purple"}}>제 이름은 하주은입니다.</p>
+        </>
+    );
+  }
